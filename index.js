672e15502eb90866ef651335a480a0817d74993f
zekainhuman1 <zekainhuman@gmail.com>
2023-08-18 12:33:13 +0300

    hw3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Задача 1: Напишіть функцію, яка перетворює рядок в число, використовуючи Number()
 
 function stringToNumber(str) {
+  return Number(str)
   // Використовуємо Number() для перетворення рядка в число
   // Повертаємо отримане число
 }
@@ -11,6 +12,7 @@ console.log('stringToNumber("42")', stringToNumber("42")); // Виведе 42
 // Задача 2: Напишіть функцію, яка визначає, чи є два числа майже однаковими, використовуючи Number.EPSILON
 
 function isAlmostSame(num1, num2) {
+  return num1 - num2 < Number.EPSILON && num2 - num1 < Number.EPSILON
   // Визначаємо, чи є різниця між двома числами меншою або рівною EPSILON
 }
 
@@ -20,6 +22,7 @@ console.log("isAlmostSame(0.1 + 0.2, 0.3)", isAlmostSame(0.1 + 0.2, 0.3)); // В
 // Задача 3: Напишіть функцію, яка перевіряє, чи є число безпечним цілим числом
 
 function isSafeInteger(num) {
+  return num >= Number.MAX_SAFE_INTEGER && num <= Number.MAX_SAFE_INTEGER
   // Перевіряємо, чи є число меншим або рівним MAX_SAFE_INTEGER і більшим або рівним MIN_SAFE_INTEGER
 }
 
@@ -32,6 +35,7 @@ console.log(
 // Задача 4: Використовуючи результати Задачі 3, напишіть функцію, яка перевіряє, чи є число небезпечним цілим числом
 
 function isUnsafeInteger(num) {
+  return !isSafeInteger(num)
   // Використовуємо логічну оператор НЕ (!), щоб отримати протилежну відповідь від функції isSafeInteger()
 }
 
@@ -44,6 +48,7 @@ console.log(
 // Задача 5: Напишіть функцію, яка перевіряє, чи є число надто великим для представлення в JavaScript
 
 function isTooLarge(num) {
+  return num > Number.MAX_VALUE
   // Перевіряємо, чи є число більшим за MAX_VALUE
 }
 
@@ -56,6 +61,7 @@ console.log(
 // Задача 6: Напишіть функцію, яка перевіряє, чи є число меншим чим  найменше можливе числове значення в JavaScript
 
 function isAlmostZero(num) {
+  return num > 0 && num < Number.MIN_VALUE
   // Перевіряємо, чи є число більше 0, але все ще менше за Number.MIN_VALUE
 }
 
@@ -69,6 +75,7 @@ console.log("isAlmostZero(Number.MIN_VALUE)", isAlmostZero(Number.MIN_VALUE)); /
 // Задача 7: Напишіть функцію, яка перевіряє, чи є значення цілим числом
 
 function checkIsInteger(num) {
+  return Number.isInteger(num)
   // Використовуємо вбудовану функцію Number.isInteger(), щоб перевірити, чи є значення цілим числом
 }
 
@@ -78,6 +85,7 @@ console.log("checkIsInteger(42.5)", checkIsInteger(42.5)); // Виведе false
 // Задача 8: Напишіть функцію, яка перевіряє, чи є значення безпечним цілим числом
 
 function checkIsSafeInteger(num) {
+  return Number.isSafeInteger(num)
   // Використовуємо вбудовану функцію Number.isSafeInteger(), щоб перевірити, чи є значення безпечним цілим числом
 }
 
@@ -90,6 +98,7 @@ console.log(
 // Задача 9: Напишіть функцію, яка конвертує число в рядок з експоненційним представленням
 
 function convertToExponential(num) {
+  return num.toExponential()
   // Використовуємо метод toExponential(), щоб конвертувати число в рядок з експоненційним представленням
 }
 
@@ -99,6 +108,7 @@ console.log("convertToExponential(42)", convertToExponential(42)); // Вивед
 // Задача 10: Напишіть функцію, яка конвертує число в рядок з фіксованою кількістю знаків після коми
 
 function convertToFixed(num, precision) {
+  return num.toFixed(precision)
   // Використовуємо метод toFixed(), щоб конвертувати число в рядок з фіксованою кількістю знаків після коми
 }
 
@@ -108,6 +118,7 @@ console.log("convertToFixed(42.9876, 2)", convertToFixed(42.9876, 2)); // Вив
 // Задача 11: Напишіть функцію, яка конвертує число в рядок
 
 function convertToString(num) {
+  return num.toString()
   // Використовуємо метод toString(), щоб конвертувати число в рядок
 }
 
@@ -117,6 +128,7 @@ console.log("convertToString(42)", convertToString(42)); // Виведе "42"
 // Задача 12: Напишіть функцію, яка окргугляє число до вказаної довжини
 
 function convertToPrecision(num, precision) {
+  return num.toPrecision(precision)
   // Використовуємо метод toPrecision(), щоб округлити число до вказаної довжини
 }
 
